6a03d67f792b33e04a498a94e986f5c70fcbef7e
Hendrik Bergunde <hbergunde@gmx.de>
2013-04-24 18:49:18 +0200

    add barebone reader module

---
+++ lib/reader.rb
@@ -0,0 +1,23 @@
+# encoding: UTF-8
+require 'digest/sha1'
+
+module Reader
+  attr_reader :items
+
+  protected
+
+  def initialize *args
+    @items = []
+  end
+
+  def add_ids
+    @items.each do |item|
+      item[:id] = digest item.to_s
+    end
+  end
+
+  private
+  def digest string
+    Digest::SHA1.hexdigest string
+  end
+end
