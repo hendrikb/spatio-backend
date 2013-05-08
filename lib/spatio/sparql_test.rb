ff9ea8435190a49f340522c121fbbba66fad689b
rschaden <rschaden@gmail.com>
2013-05-09 00:14:06 +0200

    also include top level cities like Berlin

--- lib/spatio/sparql_test.rb
+++ lib/spatio/sparql_test.rb
@@ -4,32 +4,37 @@ module Spatio
   module SparqlTest
     extend self
 
+
     def cities
-      query = '
-      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-      PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-
-      SELECT DISTINCT ?name {
-      ?city dcterms:subject ?category.
+      query_string = ' SELECT DISTINCT ?name WHERE {
       ?city rdfs:label ?name.
+      { ?city dcterms:subject ?category.
       ?category skos:broader <http://de.dbpedia.org/resource/Kategorie:Kreisfreie_Stadt_in_Deutschland>.
-      FILTER (!(?category = <http://de.dbpedia.org/resource/Kategorie:Ehemalige_kreisfreie_Stadt_in_Deutschland>) )
-      }'
+      FILTER (!(?category = <http://de.dbpedia.org/resource/Kategorie:Ehemalige_kreisfreie_Stadt_in_Deutschland>)) }
+      UNION
+      { ?city dcterms:subject <http://de.dbpedia.org/resource/Kategorie:Kreisfreie_Stadt_in_Deutschland>}
+    }'
 
-      sparql_client.query(query).map{ |res| res[:name].to_s }
+      query(query_string).reject { |city| city =~/Liste/ }
     end
 
     def states
-      query = '
-      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-      PREFIX dbprop: <http://de.dbpedia.org/property/>
-
-      SELECT DISTINCT ?name {
+      query_string = 'SELECT DISTINCT ?name {
       ?state rdfs:label ?name.
       ?state dcterms:subject <http://de.dbpedia.org/resource/Kategorie:Bundesland_(Deutschland)> }'
 
+      query query_string
+    end
+
+    PREFIXES = 'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX skos: <http://www.w3.org/2004/02/skos/core#>'
+
+    private
+
+    def query(query)
       sparql_client.query(query).map{ |res| res[:name].to_s }
     end
+
     def sparql_client
       SPARQL::Client.new('http://de.dbpedia.org/sparql')
     end
