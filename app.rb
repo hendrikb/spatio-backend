bec0efca9533271dd3e7445a2a195a1a4d453ce1
Hendrik Bergunde <hbergunde@gmx.de>
2013-05-11 01:05:15 +0200

    add format_definition_index

--- app.rb
+++ app.rb
@@ -1,10 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader' if settings.environment == :development
 
+require 'sinatra/activerecord'
+require './lib/models/format_definition'
+
 APP_ROOT = settings.root
 
 get '/' do
   haml :index
 end
 
+get '/format_definition' do
+  @format_definitions = FormatDefinition.all
+  haml :format_definition_index
 end
