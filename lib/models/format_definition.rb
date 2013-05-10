5bcf4b1bf0370c34763aa7549fc9d4c8650d8616
Hendrik Bergunde <hbergunde@gmx.de>
2013-05-11 00:57:57 +0200

    clean accessors

--- lib/models/format_definition.rb
+++ lib/models/format_definition.rb
@@ -1,6 +1,4 @@
 class FormatDefinition < ActiveRecord::Base
-  attr_accessor :name, :importer_class, :importer_parameters
-
   validates_presence_of :name
   validates_presence_of :importer_class
 end
