019fb797b1488fafddc83a6c0b6ca5f25c3f6b3c
rschaden <rschaden@gmail.com>
2013-05-11 19:36:38 +0200

    ensure validity of polygons

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,6 +25,7 @@ COMMUNITY_QUERY = <<-sql
   WHERE admin_level = '8'
   AND planet_osm_polygon.name IS NOT NULL
   AND planet_osm_polygon.name <> '?'
+  AND ST_ISVALID(planet_osm_polygon.way)
   GROUP BY planet_osm_polygon.name, states.id
 sql
 
@@ -41,6 +42,7 @@ def initialize_communities
       FROM planet_osm_polygon
       INNER JOIN states ON ST_CONTAINS(states.area, ST_TRANSFORM(way, 3785))
       WHERE planet_osm_polygon.name= '#{city}'
+      AND ST_ISVALID(planet_osm_polygon.way)
       GROUP BY planet_osm_polygon.name, states.id")
   end
 end
