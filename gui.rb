10702069ace4e826408e0fcb3b0558c3c4b7e7d7
Hendrik Bergunde <hbergunde@gmx.de>
2013-05-11 20:37:49 +0200

    add api_url variable

--- gui.rb
+++ gui.rb
@@ -4,6 +4,8 @@ require 'sinatra/reloader' if settings.environment == :development
 set :views, Proc.new { File.join(root, "views_gui") }
 set :public_folder, File.dirname(__FILE__) + '/public_gui'
 
+set :api_url, "http://127.0.0.1:4567/api"
+
 
 get '/' do
   haml :index
