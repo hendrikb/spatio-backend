373f6a4261af42f22d54b96ea3318e7cb8d8538a
Hendrik Bergunde <hbergunde@gmx.de>
2013-05-11 02:19:06 +0200

    fix path

--- gui.rb
+++ gui.rb
@@ -9,7 +9,7 @@ get '/' do
   haml :index
 end
 
-get '/v/format_definition' do
+get '/format_definition' do
   #TODO This is forbidden, this must be a JSON-API call
   @format_definitions = FormatDefinition.all
   haml :format_definition_index
