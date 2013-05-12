f27cc55692ef09907c8d8e10007a686fc151aa98
rschaden <rschaden@gmail.com>
2013-05-12 21:02:47 +0200

    basic State Parser implementation

--- lib/spatio/parser/state.rb
+++ lib/spatio/parser/state.rb
@@ -4,6 +4,9 @@ module Spatio
   module Parser
     module State
       def self.perform(location_string)
+        ::State.all.select do |community|
+          location_string.include?(community.name)
+        end.map(&:name)
       end
     end
   end
--- spec/lib/spatio/parser/state_spec.rb
+++ spec/lib/spatio/parser/state_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper'
 
 describe Spatio::Parser::State do
   it { should respond_to :perform }
+
+  it 'finds state' do
+    FactoryGirl.create(:state, name: 'Bayern')
+    subject.perform('Test in Bayern').should eq ['Bayern']
+  end
+
+  it 'works without state' do
+    subject.perform('Foobar').should be_empty
+  end
 end
