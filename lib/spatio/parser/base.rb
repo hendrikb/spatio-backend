9b14af2073bf81bcf9e53bba2905fee84b409911
rschaden <rschaden@gmail.com>
2013-05-12 20:39:55 +0200

    parse streets, districts, cities and states

--- lib/spatio/parser/base.rb
+++ lib/spatio/parser/base.rb
@@ -9,23 +9,56 @@ require 'spatio/parser/state'
 module Spatio
   module Parser
     class Base
-      attr_reader :location_string
+      attr_reader :location_string, :context, :result
 
-      def self.perform(location_string)
-        new(location_string).perform
+      def self.perform(location_string, context = '')
+        new(location_string, context).perform
       end
 
       def perform
-        result = {}
-        [:streets, :cities].each do |parser|
-          result[parser] = constantize(parser).perform(location_string)
-        end
+        resolve_context
+
+        parse_location_string
+
         result
       end
 
       private
-      def initialize(location_string)
-        @location_string = location_string
+      def initialize(location_string, context)
+        @location_string, @context  = location_string, context
+        @result = { }
+      end
+
+      def resolve_context
+        [:states, :cities].each do |parser|
+          tmp = constantize(parser).perform(context) if context.present?
+          result[parser] = tmp if tmp
+        end
+      end
+
+      def parse_location_string
+        parse_states
+        parse_cities
+        parse_districts
+        parse_streets
+      end
+
+      def parse_states
+        return if result.has_key?(:states)
+        result[:states] = Spatio::Parser::State.perform(location_string)
+      end
+
+      def parse_cities
+        return if result.has_key?(:cities)
+        result[:cities] = Spatio::Parser::City.perform(location_string)
+      end
+
+      def parse_districts
+        result[:districts] = Spatio::Parser::District.perform(location_string, result[:cities])
+      end
+
+      def parse_streets
+        result[:streets] = Spatio::Parser::Street.perform(location_string)
       end
 
       def constantize(class_name)
--- spec/lib/spatio/parser/base_spec.rb
+++ spec/lib/spatio/parser/base_spec.rb
@@ -5,6 +5,7 @@ describe Spatio::Parser::Base do
 
   context '.perform' do
     let(:location_string) { 'Musterstrasse 127 abc Berlin' }
+    let(:city) { 'Ulm' }
 
     it 'calls Streets.perform' do
       Spatio::Parser::Street.should_receive(:perform).with(location_string)
@@ -23,9 +24,42 @@ describe Spatio::Parser::Base do
     end
 
     it 'returns a hash with cities set' do
-      city = 'Berlin'
       Spatio::Parser::City.stub(:perform).and_return [city]
       subject.perform(location_string)[:cities].should eq [city]
+    end
+
+    it 'calls District.perform with city' do
+      Spatio::Parser::City.stub(:perform).and_return [city]
+      Spatio::Parser::District.should_receive(:perform).with(location_string, [city])
+      subject.perform(location_string)
+    end
+
+    it 'calls State.perform' do
+      Spatio::Parser::State.should_receive(:perform).with(location_string)
+      subject.perform(location_string)
+    end
+
+    context 'context resolution' do
+      it 'correctly sets city' do
+        Spatio::Parser::City.should_receive(:perform).with(city).and_return [city]
+        subject.perform('Musterstrasse', city)[:cities].should eq [city]
+      end
+
+      it 'correctly sets state' do
+        state = 'Bayern'
+        Spatio::Parser::State.should_receive(:perform).with(state).and_return [state]
+        subject.perform('Musterstrasse', state)[:states].should eq [state]
+      end
+
+      it 'sets both state and city for Berlin' do
+        city = 'Berlin'
+        Spatio::Parser::State.should_receive(:perform).with(city).and_return [city]
+        Spatio::Parser::City.should_receive(:perform).with(city).and_return [city]
+        result = subject.perform('Musterstrasse', city)
+
+        result[:states].should eq [city]
+        result[:cities].should eq [city]
+      end
     end
   end
 end
