f1464e78ea3aead8ced5857b36cc9e3a0169b2d2
rschaden <rschaden@gmail.com>
2013-05-12 21:00:36 +0200

    basic City Parser implementation

--- lib/spatio/parser/city.rb
+++ lib/spatio/parser/city.rb
@@ -4,6 +4,9 @@ module Spatio
   module Parser
     module City
       def self.perform(location_string)
+        Community.all.select do |community|
+          location_string.include?(community.name)
+        end.map(&:name)
       end
     end
   end
--- spec/lib/spatio/parser/city_spec.rb
+++ spec/lib/spatio/parser/city_spec.rb
@@ -2,4 +2,13 @@
 
 describe Spatio::Parser::City do
   it { should respond_to :perform }
+
+  it 'finds city' do
+    FactoryGirl.create(:community, name: 'Berlin')
+    subject.perform('Test in Berlin').should eq ['Berlin']
+  end
+
+  it 'works without city' do
+    subject.perform('Foobar').should be_empty
+  end
 end
