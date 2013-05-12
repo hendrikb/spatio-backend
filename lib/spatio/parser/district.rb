36ae32ca19017cfd3434e997e1e80b4d75d60620
rschaden <rschaden@gmail.com>
2013-05-12 13:58:15 +0200

    add basic district parsing

--- lib/spatio/parser/district.rb
+++ lib/spatio/parser/district.rb
@@ -3,7 +3,14 @@
 module Spatio
   module Parser
     module District
-      def self.perform(location_string)
+      def self.perform(location_string, city)
+        community = Community.find_by_name(city)
+        return [] unless community
+        districts = ::District.where(community_id: community.id)
+
+        districts.select do |district|
+          location_string.include?(district.name)
+        end.map(&:name)
       end
     end
   end
--- spec/lib/spatio/parser/district_spec.rb
+++ spec/lib/spatio/parser/district_spec.rb
@@ -2,4 +2,31 @@ require 'spec_helper'
 
 describe Spatio::Parser::District do
   it { should respond_to :perform }
+
+
+  context 'district in right city' do
+    let(:city) { FactoryGirl.create(:community) }
+    let!(:district) { FactoryGirl.create(:district, name: 'Mitte', community: city) }
+
+    it 'finds district' do
+      subject.perform('Test in Mitte', city.name).should eq ['Mitte']
+    end
+
+    it 'finds multiple districts' do
+      FactoryGirl.create(:district, name: 'Pankow', community: city)
+      subject.perform('Test in Pankow/Mitte', city.name).should =~ ['Mitte', 'Pankow']
+    end
+  end
+
+  it 'ignores district of other cities' do
+    city = FactoryGirl.create(:community)
+    other_city = FactoryGirl.create(:community, name: 'Hamburg')
+    FactoryGirl.create(:district, name: 'Mitte', community: other_city)
+
+    subject.perform('Test in Mitte', city.name).should be_empty
+  end
+
+  it 'does not fail with non existing city' do
+    expect { subject.perform('Test in Mitte', 'Test123') }.not_to raise_error
+  end
 end
