b9b9e408a093faec26d05cb680971f7f5e988190
Hendrik Bergunde <hbergunde@gmx.de>
2013-05-12 17:53:30 +0200

    add missing response_is_json

--- api.rb
+++ api.rb
@@ -54,6 +54,15 @@ get '/api/format_definition/' do
 end
 
 
+
+def response_is_json
+  response.headers["Access-Control-Allow-Origin"] = "*"
+  # response.headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, DELETE, PUT'
+  # response.headers['Access-Control-Max-Age'] = '1000'
+  # response.headers['Access-Control-Allow-Headers'] = 'origin, x-csrftoken, content-type, accept'
+  content_type :json
+end
+
 def okay
   { "status" => "ok"  }.to_json
 end
