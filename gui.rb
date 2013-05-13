4fa744614a9aca9c211500ff5c76f304a771ca6e
Hendrik Bergunde <hbergunde@gmx.de>
2013-05-13 17:52:20 +0200

    move api_url a little to the top

--- gui.rb
+++ gui.rb
@@ -1,11 +1,10 @@
 require 'sinatra'
 require 'sinatra/reloader' if settings.environment == :development
 
-set :views, Proc.new { File.join(root, "views_gui") }
-set :public_folder, File.dirname(__FILE__) + '/public_gui'
-
 set :api_url, "http://127.0.0.1:4567/api"
 
+set :views, Proc.new { File.join(root, "views_gui") }
+set :public_folder, File.dirname(__FILE__) + '/public_gui'
 
 get '/' do
   haml :index
