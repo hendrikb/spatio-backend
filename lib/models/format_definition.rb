e37b9f15733483c26d425678fe04ea96c0ea7916
Hendrik Bergunde <hbergunde@gmx.de>
2013-05-13 18:06:46 +0200

    provide klass of FormatDefinition

--- lib/models/format_definition.rb
+++ lib/models/format_definition.rb
@@ -1,4 +1,11 @@
 class FormatDefinition < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :importer_class
+
+  def klass
+    require './lib/spatio'
+    require './lib/spatio/reader'
+    Dir.glob("./lib/spatio/reader/*.rb").each { |file| require file }
+    eval "Spatio::Reader::#{importer_class}"
+  end
 end
