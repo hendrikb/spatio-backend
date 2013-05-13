4929799c0c72cb93fc9d7fb78c0e35e67aaa9139
Hendrik Bergunde <hbergunde@gmx.de>
2013-05-13 16:50:46 +0200

    kill data input via JSON, HTTP POST is sufficient and more robust

--- api.rb
+++ api.rb
@@ -20,17 +20,14 @@ post '/api/format_definition/new' do
   Dir.glob("./lib/spatio/reader/*.rb").each { |file| require file }
 
   begin
-    data = JSON.parse request.body.read
-    klass = eval "Spatio::Reader::#{data['importer_class']}"
+    klass = eval "Spatio::Reader::#{params['importer_class']}"
   rescue RuntimeError
-    return json_err "Parser class Spatio::Reader::#{data['importer_class']} not found"
-  rescue JSON::ParserError
-    return json_err "Invalid JSON"
+    return json_err "Parser class Spatio::Reader::#{params['importer_class']} not found"
   end
 
-  format_definition= FormatDefinition.new name: data["name"],
-    importer_class: data["importer_class"],
-    importer_parameters: data["importer_parameters"]
+  format_definition= FormatDefinition.new name: params["name"],
+    importer_class: params["importer_class"],
+    importer_parameters: params["importer_parameters"]
 
   begin
     if format_definition.save
