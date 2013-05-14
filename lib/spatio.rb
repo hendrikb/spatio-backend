adb2a6c50449ff3a3fa06e3ca72eff235b0cc7db
rschaden <rschaden@gmail.com>
2013-05-14 22:32:31 +0200

    add Geocode stub

--- lib/spatio.rb
+++ lib/spatio.rb
@@ -2,6 +2,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'spatio/parser'
 require 'spatio/reader'
+require 'spatio/geocode'
 require 'models'
 
 require './config/initializers/geocoder'
---
+++ spec/lib/spatio/geocode_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Spatio::Geocode do
+  it { should respond_to :perform }
+end
