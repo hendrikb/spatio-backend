9d043344012488ebae2a6a5d6e038f7326aced3d
rschaden <rschaden@gmail.com>
2013-05-18 01:29:28 +0200

    implement basic street geocoding

--- lib/spatio/geocode/street.rb
+++ lib/spatio/geocode/street.rb
@@ -1,9 +1,59 @@
-# endocing; utf-8
+# encoding; utf-8
+require 'active_support/core_ext/string/filters'
 
 module Spatio
   module Geocode
+    COUNTRY = 'Germany'
+
     class Street
+      attr_reader :streets, :districts, :cities
+
       def self.perform(locations)
+        new(locations).perform
+      end
+
+      def perform
+        return unless streets
+        result = with_district_and_city if districts.present? && cities.present?
+        return result if result
+        with_city if cities.present?
+      end
+
+      private
+      def initialize(locations)
+        @streets   = locations[:streets]
+        @districts = locations[:districts]
+        @cities    = locations[:cities]
+      end
+
+      def with_district_and_city
+        streets.each do |street|
+          districts.each do |district|
+            cities.each do |city|
+              result = geocode(streets.first, districts.first, cities.first)
+              return result if result
+            end
+          end
+        end
+      end
+
+      def with_city
+        streets.each do |street|
+          cities.each do |city|
+            result = geocode(streets.first, '', cities.first)
+            return result if result
+          end
+        end
+      end
+
+      def geocode(street, district = nil, city = nil)
+        coordinates = Geocoder.coordinates(location_string(street,district, city))
+        lonlat = ::RGeo::Geographic.simple_mercator_factory.point(coordinates.second, coordinates.first) if coordinates
+        lonlat.projection if lonlat
+      end
+
+      def location_string(street, district = nil, city = nil)
+        "#{street} #{district} #{city} #{COUNTRY}".squish
       end
     end
   end
--- spec/lib/spatio/geocode/street_spec.rb
+++ spec/lib/spatio/geocode/street_spec.rb
@@ -2,5 +2,44 @@ require 'spec_helper'
 
 describe Spatio::Geocode::Street do
   subject { Spatio::Geocode::Street }
+  let(:street) { 'Turmstrasse' }
+  let(:district) { 'Mitte'  }
+  let(:city) { 'Berlin' }
+  let(:coordinates) { [53, 11] }
+
+  def location_string(*args)
+    result = ""
+    args.each { |arg| result = result + " #{arg}" }
+    (result + " #{Spatio::Geocode::COUNTRY} ").strip
+  end
+
   it { should respond_to :perform }
+
+  it 'returns nil without streets' do
+    Spatio::Geocode::Street.perform({ city: [city] }).should be nil
+  end
+
+  it 'calls Geocoder with street district and city' do
+    Geocoder.should_receive(:coordinates).with(location_string(street, district, city)).
+      and_return coordinates
+    Spatio::Geocode::Street.perform({ streets: [street],
+                                      districts: [district],
+                                      cities: [city]})
+  end
+
+  it 'calls Geocoder with street city' do
+    Geocoder.should_receive(:coordinates).with(location_string(street, city)).
+      and_return coordinates
+    Spatio::Geocode::Street.perform({ streets: [street],
+                                      districts: [],
+                                      cities: [city]})
+  end
+
+  it 'returns mercator point', :vcr do
+    Spatio::Geocode::Street.perform({ streets: [street],
+                                      districts: [],
+                                      cities: [city]}).should be_kind_of RGeo::Geos::CAPIPointImpl
+
+
+  end
 end
