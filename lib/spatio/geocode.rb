66f35c491a0e2e938281d09d4ba6b9bfb12e97e2
rschaden <rschaden@gmail.com>
2013-05-18 11:44:19 +0200

    implement Geocode.perform

--- lib/spatio/geocode.rb
+++ lib/spatio/geocode.rb
@@ -5,6 +5,9 @@ require_relative 'geocode/osm_data'
 module Spatio
   module Geocode
     def self.perform(locations)
+      result = Spatio::Geocode::Street.perform(locations)
+      return result if result.present?
+      Spatio::Geocode::OsmData.perform(locations)
     end
   end
 end
