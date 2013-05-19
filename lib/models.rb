aecd8ecb36774fb3759c67f61c9183f5e76e1f63
rschaden <rschaden@gmail.com>
2013-05-19 13:42:59 +0200

    initialize database connection in Spatio instead of models

--- lib/models.rb
+++ lib/models.rb
@@ -2,14 +2,6 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require_relative  'spatio'
 
-ENV['RACK_ENV'] ||= 'development'
-
-def conf(file)
-  YAML.load_file('config/' + file).fetch(settings.environment.to_s)
-end
-
-ActiveRecord::Base.establish_connection conf('database.yml')
-
 require_relative 'models/state.rb'
 require_relative 'models/community.rb'
 require_relative 'models/district.rb'
--- lib/spatio.rb
+++ lib/spatio.rb
@@ -1,13 +1,23 @@
+require './config/initializers/geocoder'
+require 'rgeo'
+require 'sinatra'
+require 'sinatra/activerecord'
+
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'spatio/parser'
 require 'spatio/reader'
 require 'spatio/geocode'
 
-require './config/initializers/geocoder'
-require 'rgeo'
+
+ENV['RACK_ENV'] ||= 'development'
+
+def conf(file)
+  YAML.load_file('config/' + file).fetch(settings.environment.to_s)
+end
 
 module Spatio
   GEOFACTORY = ::RGeo::Geographic.simple_mercator_factory.projection_factory
 end
 
+ActiveRecord::Base.establish_connection conf('database.yml')
 require 'models'
