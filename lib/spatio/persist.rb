ac23d0548cbcc235f2ecf62c8826e5f13d27b416
rschaden <rschaden@gmail.com>
2013-05-20 14:40:32 +0200

    add Persist stub

---
+++ lib/spatio/persist.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+module Spatio
+  module Persist
+    extend self
+
+    def perform(table_name, data)
+    end
+  end
+end
---
+++ spec/lib/spatio/persist_spec.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe Spatio::Persist do
+  it { should respond_to :perform }
+end
