b263de7944fee1ae565dbc26c47841450eda99bc
Hendrik Bergunde <hbergunde@gmx.de>
2013-05-22 16:48:58 +0200

    refine import persistance model

--- lib/models/import.rb
+++ lib/models/import.rb
@@ -1,2 +1,7 @@
 class Import < ActiveRecord::Base
+  validates_presence_of :url
+  validates_presence_of :namespace
+  validates_presence_of :format_definition
+
+  belongs_to :format_definition
 end
