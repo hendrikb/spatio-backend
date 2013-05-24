2c318d0d93995b8e49436f06e42377c4a9de2e91
rschaden <rschaden@gmail.com>
2013-05-24 22:45:59 +0200

    add all_fields method to namespace that includes custom and mandatory fields

--- lib/models/namespace.rb
+++ lib/models/namespace.rb
@@ -1,5 +1,14 @@
 class Namespace < ActiveRecord::Base
   has_many :fields
+  MANDATORY_FIELD_NAMESPACE = 'Mandatory Fields'
 
   validate :name, unique: true
+
+  def all_fields
+    fields + mandatory_fields
+  end
+
+  def mandatory_fields
+    Namespace.find_by_name(MANDATORY_FIELD_NAMESPACE).fields
+  end
 end
