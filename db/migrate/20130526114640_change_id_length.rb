e466112efb16949a7ab89161381bc5afd6d3ab62
rschaden <rschaden@gmail.com>
2013-05-27 01:37:54 +0200

    add nil guard

--- db/migrate/20130526114640_change_id_length.rb
+++ db/migrate/20130526114640_change_id_length.rb
@@ -2,12 +2,12 @@ class ChangeIdLength < ActiveRecord::Migration
   def up
     namespace = Namespace.find_by_name('Mandatory Fields')
     Field.where(namespace_id: namespace.id).find_by_name('uuid').
-      update_attributes(sql_type: 'VARCHAR (40)')
+      update_attributes(sql_type: 'VARCHAR (40)') if namespace
   end
 
   def down
     namespace = Namespace.find_by_name('Mandatory Fields')
     Field.where(namespace_id: namespace.id).find_by_name('uuid').
-      update_attributes(sql_type: 'VARCHAR (32)')
+      update_attributes(sql_type: 'VARCHAR (32)') if namespace
   end
 end
