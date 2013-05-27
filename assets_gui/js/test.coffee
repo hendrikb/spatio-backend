a951cc0eaaf9f74628f9cc34d5d15a8f87727f82
Hendrik Bergunde <hbergunde@gmx.de>
2013-05-27 20:53:41 +0200

    seperate in app code and vendor code

--- assets_gui/js/test.coffee
+++
@@ -1,1 +0,0 @@
-alert 'assets geladen!'
--- gui.rb
+++ gui.rb
@@ -20,21 +20,38 @@ class App < Sinatra::Base
 
   assets {
     serve '/js',     from: 'assets_gui/js'        # Default
+    serve '/vendor_js',     from: 'assets_gui/vendor/js'        # Default
+
     serve '/css',    from: 'assets_gui/css'       # Default
+    serve '/vendor_css',     from: 'assets_gui/vendor/css'        # Default
+
     serve '/images', from: 'assets_gui/images'    # Default
+    serve '/vendor_images',     from: 'assets_gui/vendor/images'        # Default
 
     # The second parameter defines where the compressed version will be served.
     # (Note: that parameter is optional, AssetPack will figure it out.)
+    js :lib, '/js/lib.js', [
+      '/vendor_js/*.js',
+      '/vendor_js/**/*.js',
+      '/vendor_js/lib/*.js',
+      '/vendor_js/lib/**/*.js'
+    ]
+
+    css :lib, '/js/lib.css', [
+      '/vendor_css/*.css',
+      '/vendor_css/**/*.css',
+      '/vendor_css/lib/*.css',
+      '/vendor_css/lib/**/*.css'
+    ]
+
     js :app, '/js/app.js', [
-      '/js/vendor/**/*.js',
-      '/js/lib/**/*.js',
       '/js/*.coffee',
       '/js/*.js'
     ]
 
     css :app, '/css/app.css', [
-      '/css/vendor/**/*.js',
-      '/css/lib/**/*.js',
+      '/css/vendor/**/*.css',
+      '/css/lib/**/*.css',
       '/css/*.css'
     ]
 
