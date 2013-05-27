0a531d2c58344bfb90e0768215e88b34bb1a22ce
rschaden <rschaden@gmail.com>
2013-05-27 17:48:17 +0200

    set geofactory for location in persist

--- lib/spatio/persist.rb
+++ lib/spatio/persist.rb
@@ -7,6 +7,7 @@ module Spatio
     def perform(table_name, data)
       table = Class.new(ActiveRecord::Base) do
         self.table_name = table_name
+        set_rgeo_factory_for_column(:location, Spatio::GEOFACTORY)
       end
 
       row = table.new(data)
