8f599512726b35a920c47f6581bce741b5ddecfc
rschaden <rschaden@gmail.com>
2013-05-27 21:01:14 +0200

    make csv import from file work

--- lib/spatio/reader/csv.rb
+++ lib/spatio/reader/csv.rb
@@ -5,18 +5,24 @@ module Spatio
   module Reader
     class CSV
       include Spatio::Reader
+      attr_reader :options
+      DEFAULT_OPTIONS = { encoding:'utf-8', col_sep: ';'}
 
-      def self.perform file_name
-        Spatio::Reader::CSV.new(file_name).perform
+      def self.perform file_name, parameters
+        Spatio::Reader::CSV.new(file_name, parameters).perform
       end
 
-      def initialize file_name
+      def initialize file_name, parameters = {}
         @file_name = file_name
+        @options = DEFAULT_OPTIONS.merge parameters
       end
 
       def perform
         @items = []
-        @csv = ::CSV.foreach @file_name, col_sep:';',headers: :first_row, encoding: "windows-1252:UTF-8" do |row|
+        @csv = ::CSV.foreach(@file_name,
+                             col_sep: options[:col_sep],
+                             headers: :first_row,
+                             encoding: options[:encoding]) do |row|
           @items << generate_item(row)
         end
         add_ids
@@ -24,21 +30,34 @@ module Spatio
 
       private
       def generate_item row
+        title =  ""
+        options[:title_columns].each do |column|
+          title << "#{row[column]} "
+        end
         {
-          human_readable_location_in: "#{row[0]} #{row[1]} #{row[2]} #{row[3]}",
-          meta_data: {
-            bezirk: row[0],
-            standort: row[1],
-            strasse: row[2],
-            plz: convert_plz(row[3]),
-            strasse2: row[4],
-            plz2: convert_plz(row[5]),
-            ort: row[6],
-            kostenfrei: begin convert_answer_to_bool(row[7]) rescue nil end,
-            betreiber: row[8],
-            versorgnungsbereich: row[9],
-          }
+          human_readable_location_in: fill_item(row, @options[:geo_columns]),
+          meta_data: generate_metadata(row)
         }
+      end
+
+      def title
+      end
+
+      def generate_metadata row
+        result = {}
+        result[:title] = fill_item(row, @options[:title_columns])
+        options[:meta_data].each do |key, value|
+          result[key] = fill_item(row, value)
+        end
+        result
+      end
+
+      def fill_item(row, columns)
+        "".tap do |result|
+          columns.each do |column|
+            result << "#{row[column]} "
+          end
+        end.strip
       end
 
       def convert_answer_to_bool answer
