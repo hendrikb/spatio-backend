f183c28b22eaa6309dc7edb79a52135621ebdaae
rschaden <rschaden@gmail.com>
2013-05-28 01:32:58 +0200

    change url

--- contrib/demo/demo_app.rb
+++ contrib/demo/demo_app.rb
@@ -5,7 +5,7 @@ require './lib/spatio/reader/csv.rb'
 parameters = { geo_columns: [0],
                title_columns: [0],
                meta_data: { count: [2] },
-               url: 'https://dl.dropboxusercontent.com/u/23665031/historic_crimes.csv',
+               url: 'http://userpage.fu-berlin.de/rschaden/historic_crimes.csv',
                col_sep: ',' }
 stuff = Spatio::Reader::CSV.perform parameters
 
