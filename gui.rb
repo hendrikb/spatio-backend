f79aac4615dc7d865cbb1777e00afd063730c53d
Hendrik Bergunde <hbergunde@gmx.de>
2013-05-27 18:20:33 +0200

    move sinatra app to own class

--- gui.rb
+++ gui.rb
@@ -1,21 +1,65 @@
-require 'sinatra'
-require 'sinatra/reloader' if settings.environment == :development
+require 'sinatra/base'
 
-set :api_url, "http://127.0.0.1:4567/api"
+GUI_HTTP_PORT = 4568
+API_URL = "http://127.0.0.1:4567/api"
 
-set :views, Proc.new { File.join(root, "views_gui") }
-set :public_folder, File.dirname(__FILE__) + '/public_gui'
+#########################################
 
-get '/' do
-  haml :index, layout: :base
+class App < Sinatra::Base
+  require 'sinatra/reloader' if settings.environment == :development
+  require 'sinatra/assetpack'
+  set :root, File.dirname(__FILE__) # You must set app root
+
+  set :api_url, API_URL
+  set :port, GUI_HTTP_PORT
+
+  set :views, Proc.new { File.join(root, "views_gui") }
+  set :public_folder, File.dirname(__FILE__) + '/public_gui'
+
+  register Sinatra::AssetPack
+
+  assets {
+    serve '/js',     from: 'assets_gui/js'        # Default
+    serve '/css',    from: 'assets_gui/css'       # Default
+    serve '/images', from: 'assets_gui/images'    # Default
+
+    # The second parameter defines where the compressed version will be served.
+    # (Note: that parameter is optional, AssetPack will figure it out.)
+    js :app, '/js/app.js', [
+      '/js/vendor/**/*.js',
+      '/js/lib/**/*.js',
+      '/js/*.coffee',
+      '/js/*.js'
+    ]
+
+    css :app, '/css/app.css', [
+      '/css/vendor/**/*.js',
+      '/css/lib/**/*.js',
+      '/css/*.css'
+    ]
+
+    prebuild true
+    cache_dynamic_assets true
+
+  }
+
+
+
+  get '/' do
+    haml :index, layout: :base
+  end
+
+  get '/format_definition' do
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    haml :format_definition_index, layout: :base
+  end
+
+  get '/import' do
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    haml :import_index, layout: :base
+  end
+
+
+  run! if app_file == $0
 end
 
-get '/format_definition' do
-  response.headers["Access-Control-Allow-Origin"] = "*"
-  haml :format_definition_index, layout: :base
-end
-
-get '/import' do
-  response.headers["Access-Control-Allow-Origin"] = "*"
-  haml :import_index, layout: :base
-end
