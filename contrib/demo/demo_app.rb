7576bc6dc701c324616a1e8cc37ba8bf89374657
rschaden <rschaden@gmail.com>
2013-05-27 21:16:51 +0200

    demo app now opens remote historic crime data file

--- contrib/demo/demo_app.rb
+++ contrib/demo/demo_app.rb
@@ -1,27 +1,15 @@
 # encoding: UTF-8
-require './lib/reader.rb'
-require './lib/reader/csv.rb'
-require './lib/reader/csv/wlan_standorte_berlin.rb'
+require './lib/spatio/reader.rb'
+require './lib/spatio/reader/csv.rb'
 
-require './lib/reader/rss.rb'
-
-readerCSV = Reader::CSV::WlanStandorteBerlin.new 'data/csv/wlanstandort_final.csv'
-stuff = readerCSV.load
+parameters = { geo_columns: [0],
+               title_columns: [0],
+               meta_data: { count: [2] },
+               url: 'https://dl.dropboxusercontent.com/u/23665031/historic_crimes.csv',
+               col_sep: ',' }
+stuff = Spatio::Reader::CSV.perform parameters
 
 # look at stuff now!
 require 'pry'; binding.pry
 
 # ------------------------------------
-
-rss_options = {
-  parse_articles: true,
-  parse_articles_selector: '#spArticleSection',
-  input_encoding: 'iso8859-1',
-  output_encoding: 'iso8859-1'
-}
-
-SponRSS = Reader::RSS.new 'http://www.spiegel.de/reise/index.rss', rss_options
-stuff2 = SponRSS.load
-
-# look at stuff2 now!
-require 'pry'; binding.pry
