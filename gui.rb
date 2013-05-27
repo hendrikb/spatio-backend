e4a125c8c19709168bbf4ed68308c9cfd5473c59
Hendrik Bergunde <hbergunde@gmx.de>
2013-05-27 17:19:42 +0200

    move html headers to base layout

--- gui.rb
+++ gui.rb
@@ -7,15 +7,15 @@ set :views, Proc.new { File.join(root, "views_gui") }
 set :public_folder, File.dirname(__FILE__) + '/public_gui'
 
 get '/' do
-  haml :index
+  haml :index, layout: :base
 end
 
 get '/format_definition' do
   response.headers["Access-Control-Allow-Origin"] = "*"
-  haml :format_definition_index
+  haml :format_definition_index, layout: :base
 end
 
 get '/import' do
   response.headers["Access-Control-Allow-Origin"] = "*"
-  haml :import_index
+  haml :import_index, layout: :base
 end
