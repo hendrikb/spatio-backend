c0d49509574af8d741143dadba39cf33279fe481
rschaden <rschaden@gmail.com>
2013-05-28 19:47:23 +0200

    whitespace

--- spec/lib/models/format_definition_spec.rb
+++ spec/lib/models/format_definition_spec.rb
@@ -2,20 +2,22 @@
 require 'spec_helper'
 
 describe FormatDefinition do
+
   context 'fields' do
     it { should respond_to :name }
     it { should respond_to :importer_class }
     it { should respond_to :importer_parameters }
   end
+
   context 'validation' do
     it 'validates presence of name' do
-        FormatDefinition.create(importer_class: 'TestClass').save.should be_false
+      FormatDefinition.create(importer_class: 'TestClass').save.should be_false
     end
     it 'validates presence of importer_class' do
-        FormatDefinition.create(name: 'TestName').save.should be_false
+      FormatDefinition.create(name: 'TestName').save.should be_false
     end
     it 'validates works correctly' do
-        FormatDefinition.create(name: 'TestName', importer_class: 'TC').save.should be_true
+      FormatDefinition.create(name: 'TestName', importer_class: 'TC').save.should be_true
     end
   end
 end
