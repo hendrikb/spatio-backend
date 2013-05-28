db71514ba20f8513bf8bfd8d0480b85436b61321
rschaden <rschaden@gmail.com>
2013-05-28 09:26:30 +0200

    add validation for namespace before creating fields

--- lib/models/import.rb
+++ lib/models/import.rb
@@ -8,7 +8,7 @@ class Import < ActiveRecord::Base
   def create_namespace
     transaction do
       ns = Namespace.create(name: namespace, table_name: namespace.parameterize('_').tableize)
-      create_fields if format_definition.importer_parameters[:meta_data]
+      create_fields if ns.valid? && format_definition.importer_parameters[:meta_data]
       ns.create_table if ns.valid?
     end
   end
