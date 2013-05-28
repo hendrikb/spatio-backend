cd5025f97a5e9d96ff390b2de6f323191e424fab
rschaden <rschaden@gmail.com>
2013-05-28 14:54:33 +0200

    change order of steps in ImportJob

--- lib/spatio/jobs/import_job.rb
+++ lib/spatio/jobs/import_job.rb
@@ -20,13 +20,7 @@ module Spatio
 
     def perform
       LOG.info "JOB #{self.to_s}: Perfoming on #{@reader.to_s}"
-      importer_parameters = @format_definition.importer_parameters
-      importer_parameters.merge!({ url: @import.url })
-
-      entries = @reader.perform(importer_parameters)
-      entries.reject! { |e| already_existing(entries).include? e[:id] }
-      entries = add_location entries
-      save entries
+      import_entries
       LOG.info 'Finished importing'
     end
 
@@ -41,18 +35,32 @@ module Spatio
       @namespace = Namespace.find_by_name(@import.namespace)
     end
 
-    def save entries
+
+    def import_entries
+      importer_parameters = @format_definition.importer_parameters
+      importer_parameters.merge!({ url: @import.url })
+
+      entries = @reader.perform(importer_parameters)
+      entries.reject! { |e| already_existing(entries).include? e[:id] }
+
+      LOG.info "JOB #{self.to_s}: Enqueing #{entries.count} new entries"
+
       entries.each do |entry|
-        title = entry[:title]
-        begin
-          Spatio::Persist.perform(@namespace.table_name, table_row(entry))
-                                  LOG.info "Saved #{title}"
-        rescue Spatio::NoLocationError
-          LOG.warn "Could not find location: #{title}"
-        rescue
-          # TODO: differentiate between duplicate key and other errors
-          LOG.error "Could not save: #{title}"
-        end
+        entries = add_location entry
+        save entry
+      end
+    end
+
+    def save entry
+      title = entry[:title]
+      begin
+        Spatio::Persist.perform(@namespace.table_name, table_row(entry))
+        LOG.info "Saved #{title}"
+      rescue Spatio::NoLocationError
+        LOG.warn "Could not find location: #{title}"
+      rescue
+        # TODO: differentiate between duplicate key and other errors
+        LOG.error "Could not save: #{title}"
       end
     end
 
@@ -65,6 +73,16 @@ module Spatio
       }.merge entry[:meta_data]
     end
 
+    def add_location entry
+      entry[:location] = geocode entry[:human_readable_location_in]
+      entry
+    end
+
+    def geocode location_string
+      location_hash = Spatio::Parser.perform(location_string, @import.geo_context)
+      Spatio::Geocode.perform(location_hash)
+    end
+
     def already_existing entries
       namespace = @namespace
       table = Class.new(ActiveRecord::Base) do
@@ -72,19 +90,6 @@ module Spatio
       end
       uuids = entries.map { |e| e[:id] }
       @already_existing_entries ||= table.where(uuid: uuids).map(&:uuid)
-    end
-
-    def add_location entries
-      LOG.info "JOB #{self.to_s}: Enqueing #{entries.count} new entries"
-      entries.map do |entry|
-        entry[:location] = geocode entry[:human_readable_location_in]
-        entry
-      end
-    end
-
-    def geocode location_string
-      location_hash = Spatio::Parser.perform(location_string, @import.geo_context)
-      Spatio::Geocode.perform(location_hash)
     end
   end
 end
