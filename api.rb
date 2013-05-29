902a6ce0675b8b59d6f524226a62b95157cb6e8d
Hendrik Bergunde <hbergunde@gmx.de>
2013-05-29 18:40:00 +0200

    clean up

--- api.rb
+++ api.rb
@@ -19,9 +19,6 @@ require_relative 'lib/api/importer'
 
 def response_is_json
   response.headers["Access-Control-Allow-Origin"] = "*"
-  # response.headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, DELETE, PUT'
-  # response.headers['Access-Control-Max-Age'] = '1000'
-  # response.headers['Access-Control-Allow-Headers'] = 'origin, x-csrftoken, content-type, accept'
   content_type :json
 end
 
