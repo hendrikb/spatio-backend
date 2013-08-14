48fab955f63e40b8460da0b0dd8cb28dcbb3b2b7
Hendrik Bergunde <hbergunde@gmx.de>
2013-08-14 22:27:02 +0200

    implement actual csv writer

--- split.rb
+++ split.rb
@@ -1,13 +1,31 @@
-require 'rexml/document'
-include REXML
+#!/usr/bin/env ruby
 
 
-Dir.glob("*.html") do |f|
-  xmlfile = File.new(f)
-  xmldoc = Document.new(xmlfile)
-  root = xmldoc.root
 
-  xmldoc.elements.each("title") do
-     |e| puts "Movie Title : " + e
+require 'nokogiri'
+require 'csv'
+
+
+def parse
+  htmlfile = File.new "tmp/386784.html"
+  CSV.open("dumps.csv", "wb",skip_blanks: true, force_quotes: true, headers: :first_row) do |csv|
+    csv << [ "title", "district", "content" ]
+
+    Dir.glob("tmp/*.html") do |f|
+      htmlfile = File.new(f)
+      doc = Nokogiri::HTML(htmlfile)
+
+      title = begin nn(doc.css("#bomain_content h2").first.content) rescue nil end
+      district = begin nn(doc.css("#bomain_content h3").first.content) rescue "Berlin" end
+      content = begin nn(doc.css("#bomain_content .bacontent .c1").first.content) rescue nil end
+      date = begin nn(doc.css("#bomain_content .datum").first.content) rescue nil end
+      csv << [ title, district, content, date ]
+    end
   end
 end
+
+def nn str
+  str.gsub("\n"," ").strip
+end
+
+parse
