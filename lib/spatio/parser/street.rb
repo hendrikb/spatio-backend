6a8a63a4d12e7f6a03a12235b38012b457d25b74
rschaden <rschaden@gmail.com>
2013-08-18 22:24:32 +0200

    use Road table for street matching, byebye monster_regexp

--- lib/spatio/parser/street.rb
+++ lib/spatio/parser/street.rb
@@ -3,45 +3,11 @@
 module Spatio
   module Parser
     class Street
-      STREET_SIGNIFIER = ["stra(ss|ß)e",
-                          "str",
-                          "weg",
-                          "platz",
-                          "damm",
-                          "ufer",
-                          "steg",
-                          "allee",
-                          "chaussee",
-                          "markt",
-                          "ring",
-                          "brücke",
-                          "tor",
-                          "promenade",
-                          "park"]
+      attr_reader :location_string, :communities
 
-      FIX_STREETS = [
-        "Unter den Linden", "Adlergestell", "Südstern", "Grosser Stern"
-      ]
-
-      STREET_PREFIXES = [
-        "Kleine[s|r]?", "Gro(ß|ss)e[r|s]?", "St\.", "Sankt", "Alte[r|s]?", "Neue[s|r]?", "Am", "Beim", "Unterm", "Hinter de[m|r]", "An de[r|m|n]"
-      ]
-
-      def self.perform(string)
-        matches = string.scan monster_regexp
-        return [] if matches.nil?
-        matches.uniq.map(&:first).reject{ |m| m =~ /Gehweg|Richtung|(Vor|Park)platz|Elektronikmarkt/ }
-      end
-
-      private
-
-      # Awesome Regex: http://rubular.com/r/yoF4rOeKJy
-      def self.monster_regexp
-        street_signifier_lowercase = STREET_SIGNIFIER.join '|'
-        street_signifier_capitalized = STREET_SIGNIFIER.map(&:capitalize).join '|'
-        street_prefixes = STREET_PREFIXES.join '|'
-
-        /(((Alt-)\p{Lu}\p{L}+\b|(#{FIX_STREETS.join('|')}))|(((#{street_prefixes}) )?((#{street_signifier_capitalized}) de[r|s] (([0-9]+\. )?\p{Lu}\p{L}+ ?)+|\p{Lu}[\p{L}-]+(#{street_signifier_lowercase})|(\p{L}+-)+(#{street_signifier_capitalized})|\p{Lu}\p{L}+ (#{street_signifier_capitalized}))\b))/
+      def self.perform(location_string)
+        ::Road.where("? ~* concat('[[:<:]]', name, '[[:>:]]')", location_string).
+          map(&:name)
       end
     end
   end
--- spec/lib/spatio/parser/street_spec.rb
+++ spec/lib/spatio/parser/street_spec.rb
@@ -3,38 +3,24 @@
 describe Spatio::Parser::Street do
   subject { Spatio::Parser::Street }
   context '.perform' do
-    context 'fixed streets' do
-      ['Adlergestell', 'Unter den Linden'].each do |fixed_street|
-        it "matches #{fixed_street}" do
-          subject.perform(fixed_street).should include fixed_street
-        end
-      end
-    end
-    STREETS = ['Alexanderplatz', 'Kurfürstendamm', 'Turmstr',
-               'Tegeler Straße', 'Friedrichsstrasse', 'Hackescher Markt',
-               'Gendarmenmarkt', 'Kottbusser Tor', 'Strasse des 17. Juni',
-               'Allee der Kosmonauten', 'Karl-Marx-Allee', 'Alte Hamburger Strasse',
-               'Görlitzer Park', 'Alt-Moabit'
-    ]
+    STREETS = ['Alexanderplatz', 'Kurfürstendamm', 'Turmstr']
+
     context 'other streets' do
       STREETS.each do |street|
-        it "matches #{street}" do
+        it "matches #{street} in roads table" do
+          FactoryGirl.create :road, name: street
           subject.perform(street).should include street
-        end
-      end
-    end
-
-    context 'other places' do
-      ['Gehweg', 'Parkplatz'].each do |place|
-        it "does not match #{place}" do
-          subject.perform(place).should_not include place
         end
       end
     end
 
     context 'multiple streets' do
       it 'returns multiple streets' do
-        subject.perform('Turmstr/Beusselstr').should eq ['Turmstr', 'Beusselstr']
+        first_street = 'Turmstrasse'
+        second_street = 'Beusselstrasse'
+        FactoryGirl.create :road, name: first_street
+        FactoryGirl.create :road, name: second_street
+        subject.perform("#{first_street}/#{second_street}").should =~ [first_street, second_street]
       end
     end
   end
